a23d752130121ceac1b622f4401b9ad80c371662
Anand Banerjee <anand.banerjee@gmail.com>
2026-08-24 16:40:16 +1000

    build: PHASE-3-PART-52-initialise Spring Boot backend

---
+++ src/test/java/au/com/futureminds/learning/FutureMindsApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.futureminds.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureMindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
